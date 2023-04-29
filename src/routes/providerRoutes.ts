c187bd33694a64ec59bec34d5b5ba55cb6977e09
Rodrigo Hirigoyen <rohigon@hotmail.com>
2023-04-29 19:13:03 -0300

    Back Provider

--- src/routes/providerRoutes.ts
+++ src/routes/providerRoutes.ts
@@ -1,13 +1,14 @@
+//@ts-nocheck
 import { Router } from 'express';
 import providerController from '../controllers/providerController';
 import auth from '../shared/authorization_middleware/auth';
 
 const router = Router();
 
-//@ts-ignore
+
 router.post('/add', auth.verifyToken, auth.authRolePermissions(['ADMIN']), providerController.addProvider);
-router.put('/:providerId', providerController.updateProvider);
-router.delete('/:name', providerController.deleteProvider);
+router.put('/:providerId', auth.verifyToken, auth.authRolePermissions(['ADMIN']), providerController.updateProvider);
+router.delete('/:name', auth.verifyToken, auth.authRolePermissions(['ADMIN']), providerController.deleteProvider);
 router.get('/', providerController.getProviders);
 router.get('/:providerId/products', providerController.getProductsByProvider);
 export default router;
