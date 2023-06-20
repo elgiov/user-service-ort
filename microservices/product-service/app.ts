dbeb62ea7a32a1bb23fc754ac4799edaf6338041
Giovanni <gioghisellini@gmail.com>
2023-06-19 23:34:46 -0300

    CHORE: Remove files

--- microservices/product-service/app.ts
+++ microservices/product-service/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import productRoutes from './routes/productRoutes';
 import { connectDB } from '../sales-service/src/database';
+import axios from 'axios';
 
 const app = express();
 app.use(cors());
@@ -15,6 +16,25 @@ app.use(function (req, res, next) {
         'Access-Control-Allow-Headers',
         'Referrer-Policy, Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization, cache-control'
     );
+    next();
+});
+
+app.use((req, res, next) => {
+    const start = Date.now();
+
+    res.on('finish', () => {
+        const duration = Date.now() - start;
+        const logData = {
+            method: req.method,
+            path: req.originalUrl,
+            duration: duration,
+            status: res.statusCode,
+            date: new Date()
+        };
+
+        axios.post('http://127.0.0.1:5000/log', logData).catch((err) => console.error(err));
+    });
+
     next();
 });
 
